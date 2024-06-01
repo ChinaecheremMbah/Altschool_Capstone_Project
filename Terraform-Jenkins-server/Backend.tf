a413e3532a07b99edd6292532f9f92b6db3fa078
chinecherem mba <chimnaecherem23@gmail.com>
2024-06-01 03:25:34 +0100

    commit

--- Terraform-Jenkins-server/Backend.tf
+++ Terraform-Jenkins-server/Backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "socks-shop-app"
     region = "eu-west-2"
-    key    = "Terraform-Jenkins-server"
+    key    = "Terraform-Jenkins-server/terraform.tfvars"
   }
 }
 
